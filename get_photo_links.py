45f782d7f971926d18f67046fa7bbe7c507bb46c
draggavv <gavrilescu.dragos@gmail.com>
2014-08-07 12:05:45 +0300

    Create get_photo_links.py

---
+++ get_photo_links.py
@@ -0,0 +1,18 @@
+import json
+from urllib2 import urlopen
+
+"photocollage.txt")
+data = json.loads(fp.read())
+fp.close()
+
+user_photos = {} # id -> [User's Name, Photo URL]
+
+for user in data["data"]:
+    print user
+    page = urlopen("http://graph.facebook.com/" + user["id"] + "?fields=picture")
+    page_data = json.loads(page.read())
+    photo_url = page_data["picture"]["data"]["url"]
+    user_photos[user["id"]] = [user["name"], photo_url]
+
+fp = open("user_photos.json", "w")
+fp.write(json.dumps(user_photos))
